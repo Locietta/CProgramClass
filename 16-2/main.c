19e765a56691f3aba77b60aef5a2b22621379f40
Locietta <2324566152@qq.com>
2020-02-28 01:12:32 +0800

    mingw make project

---
+++ 16-2/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "genlib.h"
+#include "strlib.h"
+#include "simpio.h"
+
+
+typedef struct {
+
+
+
+} *book;
+
+
+
+
+
+int main(void) {
+    
+    return 0;
+}
