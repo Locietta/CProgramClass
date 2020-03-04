bcaa4e030e4d8f0062a816f6f8a56414c66d7211
Locietta <2324566152@qq.com>
2020-03-04 13:02:31 +0800

    "currency.h" implemented

---
+++ 16-7/include/currency.h
@@ -0,0 +1,36 @@
+/**
+ * File: currency.h
+ * @Locietta
+ * -----------------------------------------------------
+ * The currency.h file defines the interface for a simple
+ * currency database.
+ */
+
+#ifndef CURRENCY_H
+#define CURRENCY_H
+
+#include <stdio.h>
+
+#define MAX_CURRENCY_NAME 15
+#define MAX_CURRENCY_SORTS 1000
+
+typedef struct {
+    char name[MAX_CURRENCY_NAME];
+    double exchangeRate;
+} * currencyT;
+
+typedef struct currencyDB_info *currencyDB;
+struct currencyDB_info {
+    currencyT currency[MAX_CURRENCY_SORTS];
+    int nCurrency;
+    void (*add)(currencyDB this, FILE *stream);
+    void (*destory)(currencyDB this);
+};
+
+currencyDB dbInit(void);
+
+void addCurrency(currencyDB this, FILE *stream);
+
+void dbDestory(currencyDB this);
+
+#endif // (!defined CURRENCY_H)
--- 16-7/lib/currency.c
+++ 16-7/lib/currency.c
@@ -1,2 +1,48 @@
-#include <stdio.h>
+/**
+ * File: currency.c
+ * @Locietta
+ * -----------------------------------------------------
+ * This file implements the currency.h interface.
+ */
 
+#include "currency.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+currencyDB dbInit(void) {
+    currencyDB newDB = (currencyDB) malloc(sizeof(struct currencyDB_info));
+    newDB->nCurrency = 0;
+    newDB->add = addCurrency;
+    newDB->destory = dbDestory;
+}
+
+void addCurrency(currencyDB this, FILE *stream) {
+    char newName[MAX_CURRENCY_NAME] = "";
+    double newExRate;
+    fscanf(stream, "%s %lf", newName, &newExRate);
+    for (int i = 0; newName != '\0'; ++i) { // ignore alphabet case
+        newName[i] = tolower(newName);
+    }
+
+    for (int i = 0; i < this->nCurrency; ++i) {
+        if (!strcmp(newName, this->currency[i]->name)) {
+            this->currency[i]->exchangeRate = newExRate;
+            return;
+        }
+    }
+
+    currencyT newCurrency = (currencyT) malloc(sizeof(currencyT));
+    strcpy(newCurrency->name, newName);
+    newCurrency->exchangeRate = newExRate;
+    this->currency[this->nCurrency] = newCurrency;
+    this->nCurrency++;
+}
+
+void dbDestory(currencyDB this) {
+    int currencyNum = this->nCurrency;
+    for (int i = 0; i < currencyNum; ++i) {
+        free(this->currency[i]);
+    }
+    free(this);
+}
